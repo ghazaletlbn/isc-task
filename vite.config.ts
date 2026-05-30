dd79d5bf5d510eb0489a1641f7ad5111d5b9fc8a
ghazaletlbn <ghazal7988@gmail.com>
2026-05-30 14:59:59 +0330

    chore(vite): configure api proxy and load env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd())
+
+    const API_TARGET = env.VITE_API_URL || 'http://localhost:3020'
+
+    return {
+        plugins: [react(), tailwindcss()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: API_TARGET,
+                    changeOrigin: true,
+                    rewrite: (path: string) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+    }
 })
