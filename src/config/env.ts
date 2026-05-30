a60a974b64d5a3c199688f16d0ebfea6b590b24b
ghazaletlbn <ghazal7988@gmail.com>
2026-05-30 17:43:09 +0330

    chore(config): introduce proxy base url environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,9 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-if (!API_URL) {
-    throw new Error("VITE_API_URL is not defined in environment variables.");
+if (!API_BASE_URL) {
+    throw new Error("VITE_API_BASE_URL is not defined in environment variables.");
 }
 
-export const env = {
-    API_URL,
-};
+export const env = Object.freeze({
+    API_BASE_URL,
+});
